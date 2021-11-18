66a8d939848b89b0faac360eb155ffe1400a5753
alangibson <alan.gibson@gmail.com>
2021-11-18 18:17:55 +0100

    Improving update script

--- server/bin/update.sh
+++ server/bin/update.sh
@@ -1,10 +1,15 @@
-#!/usr/bin/env bash -e
+#!/usr/bin/env bash
 
+#
+# Must be run from project root directory.
 #
 # https://dev.to/severo/using-webhooks-to-update-a-self-hosted-jekyll-blog-59al 
 #
 
-systemctl daemon-reload
 systemctl stop memori
+
 git pull
+sudo cp server/memori.service /etc/systemd/system/memori.service
+systemctl daemon-reload
+
 systemctl start memori
