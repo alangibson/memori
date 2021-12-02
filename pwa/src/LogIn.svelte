e550f2bd8a95b184c0c08336465271d2e3e9f3d7
alangibson <alan.gibson@gmail.com>
2021-12-02 15:00:01 +0100

    Tweaking Material UI

--- pwa/src/LogIn.svelte
+++ pwa/src/LogIn.svelte
@@ -9,10 +9,7 @@
     let token = "";
 </script>
 
-<Banner open mobileStacked content$style="max-width: max-content;">
-
-    <!-- <Icon slot="icon" class="material-icons">warning</Icon> -->
- 
+<Banner open mobileStacked>
     <Content slot="label">
         <p>
             We got a bad response from the server. Are you sure you are logged
@@ -34,81 +31,8 @@
             </Button>
         </form>
     </Content>
-
-    <!-- <Label slot="label">
-
-        We got a bad response from the server. Are you sure you are logged in?
-
-        <form method="get" action="/login">
-            <Textfield variant="outlined" bind:value={token} label="Token">
-                <Icon class="material-icons" slot="leadingIcon">key</Icon>
-                <HelperText slot="helper">Secret token</HelperText>
-              </Textfield>
-
-            <Button>Log In</Button>
-        </form>
-
-    </Label> -->
-
     <Button slot="actions">
         <Icon class="material-icons">close</Icon>
         <Label>Close</Label>
     </Button>
 </Banner>
-
-<!-- <div>
-    <div class="notification is-warning">
-        <button class="delete" />
-        <div class="container">
-            <div class="columns">
-                <div class="column">
-                    <div class="card">
-                        <div class="card-content">
-                            <p>
-                                We got a bad response from the server. Are you
-                                sure you are logged in?
-                            </p>
-                            <form method="get" action="/login">
-                                <div class="field">
-                                    <label class="label">Token</label>
-                                    <div class="control">
-                                        <input
-                                            name="token"
-                                            class="input"
-                                            type="text"
-                                            placeholder="Secret Token"
-                                        />
-                                    </div>
-                                </div>
-                                <div class="field is-grouped">
-                                    <div class="control">
-                                        <button class="button is-link"
-                                            >Log In</button
-                                        >
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-                <div class="column">
-                    <div class="card">
-                        <div class="card-content">
-                            <p>
-                                If you haven't created a Mind yet, you can
-                                create one here. Just enter a name and click
-                                Create. We'll respond with your secret token and
-                                automatically log you in.
-                            </p>
-                            <form method="POST">
-                                <label for="mindName">Name</label>
-                                <input name="mindName" />
-                                <button>Create</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> -->
