20327d567f2fafc1bd977601fa798858139b852c
Alan Gibson <alan.gibson@gmail.com>
2021-12-08 12:05:52 +0100

    Remove fontawesome

--- pwa/src/fontawesome.ts
+++
@@ -1,9 +0,0 @@
-import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { faMarker, faCamera, faMicrophone, faVideo, faLink, faFile } from '@fortawesome/free-solid-svg-icons';
-
-library.add(faMarker, faCamera, faMicrophone, faVideo, faLink, faFile)
-
-window.addEventListener('DOMContentLoaded', () => {
-  dom.i2svg()
-  dom.watch()
-})
