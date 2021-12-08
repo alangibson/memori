ddc8dba2ee7d0b9b313e628605588404ac365741
Alan Gibson <alan.gibson@gmail.com>
2021-12-08 12:04:20 +0100

    Making pm2 work on Windows

--- pm2.config.js
+++ pm2.config.js
@@ -1,46 +1,57 @@
+const path = require('path');
+const path_NODEJS = process.env.Path.split(';').filter( f => f.includes('nodejs') )[0];
+// console.log(path_NODEJS);
+// const path_NPM = path.join( path_NODEJS, 'node_modules', 'npm', 'bin', 'npm-cli.js');
+const path_NPM = './pm2-node.js'
+
 module.exports = {
     apps: [
         {
             name: 'proxy',
             cwd: './proxy',
-            script: 'npm run start -- -n test',
-            args: '',
+            script: path_NPM,
+            args: 'start -- -n test',
             // watch: ['proxy'],
             // watch: true,
             // env: {
             //     NODE_ENV: 'development',
             // },
             source_map_support: true,
+            // interpreter: 'none'
         },
         {
             name: 'server',
             cwd: './server',
-            script: 'npm run start',
-            // args: '',
+            script: path_NPM,
+            args: 'start',
             // watch: ['server'],
             // watch: true,
             // env: {
             //     NODE_ENV: 'development',
             // },
             source_map_support: true,
+            // interpreter: 'none'
         },
         {
             name: 'pwa',
             cwd: './pwa',
             // script: 'npm run start',
-            script: 'npm run dev',
-            // args: '',
+            script: path_NPM,
+            args: 'run dev',
             // watch: ['server'],
             // watch: true,
             // env: {
             //     NODE_ENV: 'development',
             // },
             source_map_support: true,
+            // interpreter: 'none'
         },
         {
             name: 'extension',
             cwd: './extension',
-            script: 'npm start'
+            script: path_NPM,
+            args: 'start',
+            // interpreter: 'none'
         }
     ]
 };
